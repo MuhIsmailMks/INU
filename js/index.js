3bc2522d5f503ce27377a5c50b8b0cfee8cce10d
muhammad ismail <muhismailmks@gmail.com>
2024-10-23 06:13:54 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
-let copyStatus = '0x85A3. . .9e40ab9'; // Teks asli
-const textRef = document.getElementById('textToCopy'); // Menggunakan ID untuk mendapatkan elemen
-
-function handleCopy() {
-  const textToCopy = textRef.getAttribute('data-text');
+let originalText = {
+    element1: '0x85A3. . .9e40ab9', // Teks asli elemen pertama
+    element2: '0x85A3. . .9e40ab9', // Teks asli elemen pertama 
+  };
   
-  // Salin teks ke clipboard
-  navigator.clipboard.writeText(textToCopy).then(() => {
-    // Ubah teks menjadi "COPIED"
-    copyStatus = 'COPIED';
-    textRef.textContent = copyStatus; // Ubah teks di elemen
+  function handleCopy(elementId) {
+    const textRef = document.getElementById(elementId); // Mendapatkan elemen berdasarkan ID
+    const textToCopy = textRef.getAttribute('data-text'); // Mendapatkan data-text dari elemen
     
-    // Setelah 2 detik, kembalikan teks ke nilai aslinya
-    setTimeout(() => {  
-    //   copyStatus = textToCopy; 
-    copyStatus = '0x85A3. . .9e40ab9';
-      textRef.textContent = copyStatus; // Kembalikan teks di elemen
-    }, 2000);
-  }).catch(err => {
-    console.error('Failed to copy text: ', err);
-  });
-}
-
-// Contoh penggunaan, tambahkan event listener
-document.getElementById('copyButton').addEventListener('click', handleCopy);
+    // Salin teks ke clipboard
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      // Ubah teks menjadi "COPIED"
+      textRef.textContent = 'COPIED'; // Ubah teks di elemen
+  
+      // Setelah 2 detik, kembalikan teks ke nilai aslinya
+      setTimeout(() => {
+        textRef.textContent = '0x85A3. . .9e40ab9'  
+      }, 2000);
+    }).catch(err => {
+      console.error('Failed to copy text: ', err);
+    });
+  }
+  
+  // Contoh penggunaan, tambahkan event listener pada kedua tombol
+  document.getElementById('copyButton1').addEventListener('click', () => handleCopy('textToCopy1'));
+  document.getElementById('copyButton2').addEventListener('click', () => handleCopy('textToCopy2'));
+  
